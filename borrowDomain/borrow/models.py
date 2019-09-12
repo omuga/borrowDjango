5a20f89ed87aafc629b20e94468295c283f9d2d2
omuga <omuga@alumnos.inf.utfsm.cl>
2019-09-12 00:31:48 -0300

    Cambios en modelos y travis.yml

--- borrowDomain/borrow/models.py
+++ borrowDomain/borrow/models.py
@@ -7,10 +7,6 @@ class Borrow(models.Model):
     fecha_inicio = models.DateField()
     fecha_termino = models.DateField()
 
-    def __init__(self,id_p,id_s,fecha_ini,fecha_ter):
-        self.id_prestamista = id_p
-        self.id_solicitante = id_s
-        self.fecha_inicio = fecha_ini
-        self.fecha_termino = fecha_ter
+
     
 
