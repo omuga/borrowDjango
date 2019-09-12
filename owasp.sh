adb82a5cbc954ea423c46f6b6e354c6a2f2f9c02
omuga <omuga@alumnos.inf.utfsm.cl>
2019-09-12 02:13:01 -0300

    Integracion OWASP

--- owasp.sh
+++ owasp.sh
@@ -1,6 +1,4 @@
-- echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-- docker build -t omuga/bmborrowedjango_web .
-- docker run -d --name bmborrowed -t omuga/bmborrowedjango_web
-- sleep 15
-after_script:
-    - docker run --name owasp --link bmborrowed -t owasp/zap2docker-weekly zap-full-scan.py -t http://bmborrowed:8001
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t omuga/bmborrowedjango_web .
+docker run -d --name bmborrowed -t omuga/bmborrowedjango_web
+sleep 15
